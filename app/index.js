2f362348ad7620de5e866423b3a1871f6628e47a
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2021-01-17 23:18:20 +0100

    Intento de cambio corregido.

--- app/index.js
+++ app/index.js
@@ -1,55 +1,41 @@
-# Creo el package.json
-npm init -yes
-
-# Instalo nodemon como dependencia solo para el desarrollo (-D)
-# Nodemos me permite ejecutar mi servidor sin necesidad de estar reiniciandolo
-npm i nodemon -D
-
-# Instalo express como dependencia de desarrollo
-npm i nodemon -D
-
-# Creo un archivo 'app.js' en la raíz, donde estará toda la lógica y funcionalidad de mi API
-
-app.js
----------------------------------------------------------------------------------
-    const express = require('express');
+const express = require('express');
 const mysql = require('mysql');
 
-# El puerto será el de nuestro hosting, y si no el que queramos en local: 3050
-const PUERTO = process.env.PUERTO || 3050;
+// El puerto será el de nuestro hosting, y si no el que queramos en local: 3050
+const PUERTO = process.env.PORT || 3050;
 
-# Creo una instancia de express
+// Creo una instancia de express
 const app = express();
 
-# Le decimos que use express.json(), que está basado e bodyParser, de esta forma podemos enviar información en JSON si lo necesitamos.
-# Nota: Para enviar JSON desde Postman activar la pestaña 'Body' > raw > Y cambiar a JSON en la derecha
+// Le decimos que use express.json(), que está basado e bodyParser, de esta forma podemos enviar información en JSON si lo necesitamos.
+// Nota: Para enviar JSON desde Postman activar la pestaña 'Body' > raw > Y cambiar a JSON en la derecha
 app.use(express.json());
 
-# MySql - Configuro la conexión
+// MySql - Configuro la conexión
 const conexion = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'api_mysql'
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'b3d1aed80d7ff3',
+    password : 'db5ce683',
+    database : 'heroku_b4585f3fb3fd27f'
 });
 
-# Ruta para probar a renderizar algo
+// Ruta para probar a renderizar algo
 app.get('/', (req, res) => {
     res.send('Bienvenido a mi API!')
 })
 
-# Obtener todos los usuarios
+// Obtener todos los usuarios
 app.get('/usuarios', (req, res) => {
 
-    # Creo mi consulta
+    // Creo mi consulta
     const sql = "SELECT * FROM usuarios"
 
-    # La ejecuto
+    // La ejecuto
     conexion.query(sql, (error, results) => {
-        # Si hay un error, lo muestro
+        // Si hay un error, lo muestro
         if (error) { throw error }
 
-        # Si hay más de un registro, los devuelvo en formato json
+        // Si hay más de un registro, los devuelvo en formato json
         if (results.length > 0) {
             res.json(results)
         } else {
@@ -59,10 +45,10 @@ app.get('/usuarios', (req, res) => {
 });
 
 app.get('/usuarios/:id', (req, res) => {
-    # Extraigo mi parámetro id del objeto params
+    // Extraigo mi parámetro id del objeto params
     const { id } = req.params
 
-    # Creo mi consulta, la ejecuto y manejo errores
+    // Creo mi consulta, la ejecuto y manejo errores
     const sql = `SELECT * FROM usuarios WHERE id=${id}`
     
     conexion.query(sql, (error, result) => {
@@ -76,24 +62,24 @@ app.get('/usuarios/:id', (req, res) => {
     })
 });
 
-# Añadir un usuario
+// Añadir un usuario
 app.post('/add', (req, res) => {
-    # Pongo SET ? en vez de VALUES() 
+    // Pongo SET ? en vez de VALUES() 
     const sql = `INSERT INTO usuarios SET ?`
 
-    # Esto es para obtener la fecha y hora actual
+    // Esto es para obtener la fecha y hora actual
     const fechaHora = { toSqlString: function() { return 'CURRENT_TIMESTAMP()'; } };
 
-    # Porque eso me permitirá pasar este objeto a la query
-    # Las claves deben llamarse como los nombres de las columnas en la BDD
-    # El método json() de express me permite en la petición recibir información extra que yo quiera en formato JSON con estas claves:
+    // Porque eso me permitirá pasar este objeto a la query
+    // Las claves deben llamarse como los nombres de las columnas en la BDD
+    // El método json() de express me permite en la petición recibir información extra que yo quiera en formato JSON con estas claves:
     const objUsuario = {
         nombre: req.body.nombre,
         edad: req.body.edad,
         alta: fechaHora
     }
 
-    # Ejecuto pasando el objeto con los datos que completarán la consulta 
+    // Ejecuto pasando el objeto con los datos que completarán la consulta 
     conexion.query(sql, objUsuario, error => {
         if (error) {
             throw error
@@ -103,15 +89,15 @@ app.post('/add', (req, res) => {
     })
 });
 
-# Editar un usuario
+// Editar un usuario
 app.put('/editar/:id', (req, res) => {
 
     const { id } = req.params
 
-    # Nombres de las claves del JSON pasado en la petición, gracias al express.json() de arriba las puedo obtener
+    // Nombres de las claves del JSON pasado en la petición, gracias al express.json() de arriba las puedo obtener
     const { nombre, edad } = req.body
 
-    # En este caso, las variables que se usan el set deben ir entre comillas simples.
+    // En este caso, las variables que se usan el set deben ir entre comillas simples.
     const sql = `UPDATE usuarios SET nombre = '${nombre}', edad = '${edad}' WHERE id = ${id}`
 
     conexion.query(sql, (error) => {
@@ -123,7 +109,7 @@ app.put('/editar/:id', (req, res) => {
     })
 });
 
-# Borrar un usuario
+// Borrar un usuario
 app.delete('/borrar/:id', (req, res) => {
     
     const { id } = req.params
@@ -138,28 +124,13 @@ app.delete('/borrar/:id', (req, res) => {
     })
 })
 
-# Realizo la conexion
+// Realizo la conexion
 conexion.connect((error) => {
     if (error) { throw error }
     console.log('La base de datos se conectó correctamente!')
 })
 
-# Iniciamos la aplicación en nuestro puerto
+// Iniciamos la aplicación en nuestro puerto
 app.listen(PUERTO, () => {
     console.log(`Servidor corriendo en el puerto ${PUERTO}`)
 })
-
----------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
